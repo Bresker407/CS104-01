2d01475ce075b08577169fe73d51c013d04bd496
Ben <146225616+Bresker407@users.noreply.github.com>
2023-10-06 08:35:54 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,15 @@
+#CS104
+#Benedetto Aiello
+#Conditions.py
+temp = int(input("What is the temperature"))
+
+if temp >= 90:
+    print("Wear Shoes")
+elif temp >= 70:
+    print("Wear Short Sleeves")
+elif temp >=50:
+    print ("Wear a Jacket")
+elif temp >=32:
+    print ("Wear a Heavy Jacket")
+else:
+    print ("Stay Inside")
